16c2daed15e8945db763aead26e31f05789c2ae1
Heitor Franco Madruga <154518862+HeitorFranco123@users.noreply.github.com>
2025-12-03 16:26:14 -0700

    Fix route definition for manager dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,13 +1,14 @@
 // routes/dashboard.js
 const express = require('express');
 const router = express.Router();
-const { requireManager } =  require('../middleware/auth');
+const { requireManager } = require('../middleware/auth');
 
-//Manager-Only dashboard  
-router.get('dashboard', requireManager, (req,res) => {
+// Manager-only dashboard
+router.get('/dashboard', requireManager, (req, res) => {
   res.render('dashboard/index', {
     title: 'Dashboard'
   });
 });
 
 module.exports = router;
+
